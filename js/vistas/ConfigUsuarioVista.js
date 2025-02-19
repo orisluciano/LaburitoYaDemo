d0b658a650e64a14cbc517658e14da596dcc253a
Luciano <orisluciano@gmail.com>
2025-02-19 16:53:58 -0300

    configUser - btnCambiarNombre

--- js/vistas/ConfigUsuarioVista.js
+++ js/vistas/ConfigUsuarioVista.js
@@ -1,8 +1,21 @@
 class ConfigUsuarioVista {
     dir = "./html/configUsuario.html";
     ids = {
-        txtSugerencia : "txtSugerencia",
-        btnEnviar : "btnEnviar"
+        txtUser : "txtUser",
+        divPassOriginal : "divPassOriginal",
+        txtPass : "txtPass",
+        divNombre : "divNombre",
+        btnNombre : "btnNombre",
+        divBtnUser : "divBtnUser",
+        btnCambiarNombre : "btnCambiarNombre",
+        btnCancelarNombre : "btnCancelarNombre",
+        divContraseña : "divContraseña",
+        btnPass : "btnPass",
+        txtNuevo : "txtNuevo",
+        txtNuevo2 : "txtNuevo2",
+        divBtnPass : "divBtnPass",
+        btnCambiarPass : "btnCambiarPass",
+        btnCancelarPass : "btnCancelarPass"
     };
 
     constructor(parameters) {
@@ -15,21 +28,68 @@ class ConfigUsuarioVista {
         let root = document.getElementById("root");
         root.innerHTML = "";
         root.innerHTML = await vista;
-        //this.cargarFunciones();
+        this.cargarFunciones();
     }
 
     cargarFunciones(){
         let esto = this;
-        let btnEnviar = document.getElementById(this.ids.btnEnviar);
-        btnEnviar.onclick = function() {
-            esto.btnEnviarOnClick();
-        }
+        let btnNombre = document.getElementById(this.ids.btnNombre);
+        btnNombre.onclick = function() {
+            esto.btnNombreOnClick();
+        };
+        let btnCambiarNombre = document.getElementById(this.ids.btnCambiarNombre);
+        btnCambiarNombre.onclick = function() {
+            esto.btnCambiarNombreOnclick();
+        };
+        let btnCancelarNombre = document.getElementById(this.ids.btnCancelarNombre);
+        btnCancelarNombre.onclick = function() {
+            esto.btnCancelarNombreOnClick();
+        };
+        let btnPass = document.getElementById(this.ids.btnPass);
+        btnPass.onclick = function() {
+            alert("");
+        };
+        let btnCambiarPass = document.getElementById(this.ids.btnCambiarPass);
+        btnCambiarPass.onclick = function() {
+            alert("");
+        };
+        let btnCancelarPass = document.getElementById(this.ids.btnCancelarPass);
+        btnCancelarPass.onclick = function() {
+            alert("");
+        };
     }
 
-    btnEnviarOnClick(){
-        let txtSugerencia = document.getElementById(this.ids.txtSugerencia);
-        console.log(txtSugerencia.value);
-        alert("Su sugerencia fue enviada. Muchas gracias");
+    btnNombreOnClick(){
+        let txtUser = document.getElementById(this.ids.txtUser);
+        txtUser.disabled = false;
+        let divPassOriginal = document.getElementById(this.ids.divPassOriginal);
+        divPassOriginal.className = "borderDivInput marginBotton20";
+        let txtPass = document.getElementById(this.ids.txtPass);
+        txtPass.innerHTML = "";
+        let divBtnUser = document.getElementById(this.ids.divBtnUser);
+        divBtnUser.className = "displayFlex marginBotton20";
+        let divNombre = document.getElementById(this.ids.divNombre);
+        divNombre.className = "displayNone";
+    }
+
+    btnCambiarNombreOnclick(){
+        alert("");
+    }
+
+    btnCancelarNombreOnClick(){
+        alert("");
+    }
+
+    btnPassOnClick(){
+        alert("");
+    }
+
+    btnCambiarPassOnclick(){
+        alert("");
+    }
+
+    btnCancelarPassOnClick(){
+        alert("");
     }
 }
 export default ConfigUsuarioVista;
