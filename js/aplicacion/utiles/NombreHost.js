6a5be39a321dda3df591bdf8b47c6a924d2bc5db
Luciano <orisluciano@gmail.com>
2025-08-14 20:22:04 -0300

    nomnrehost - correccion

--- js/aplicacion/utiles/NombreHost.js
+++ js/aplicacion/utiles/NombreHost.js
@@ -2,7 +2,7 @@ class NombreHost {
     nombre = "";
     constructor() {
         if (window.location.hostname === "localhost") {
-            this.nombre = "localhost";
+            this.nombre = "http://localhost";
         }else {
             this.nombre = "orisluciano.website";
         }
